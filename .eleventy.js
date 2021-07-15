9107b4fcc371e04cadd460a56523a82ef9ceb6af
Liang <jliang@TYCM15024.local>
2021-07-15 14:19:33 -0400

    inital step to convert to Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = (eleventyConfig) => {
+    // Copy the "assets" directory to the compiled "_site" folder.
+    eleventyConfig.addPassthroughCopy('assets');
+
+    return {
+        dir: {
+            input: './',
+            output: './_site',
+            layouts: './_layouts',
+            includes: './_includes',
+        },
+        templateFormats: [
+            'html',
+            'liquid',
+            'md',
+        ],
+    };
+};
